e001d73cbd9e06167917a5fe5732237b04136322
Ronald Velez <ronald.velez@upr.edu>
2021-12-05 15:40:54 -0400

    Changes

--- frontend/src/RoomManagement.js
+++ frontend/src/RoomManagement.js
@@ -20,9 +20,8 @@ import Axios from "axios";
             finish_time: finish_time,
         }
         const rooms = []
-        localStorage.setItem('rooms', "")
-
         let textarea = document.getElementById("textarea");
+
         function getRoomsAvailableAtTimeFrame() {
             if (rooms.length !== 0) {
                 const len = rooms.length
@@ -48,6 +47,32 @@ import Axios from "axios";
                     console.log(err)
                 })
         }
+        function getUserWhoAvailableAtTimeFrame() {
+            if (rooms.length !== 0) {
+                const len = rooms.length
+                for (let i = 0; i < len; i++) {
+                    rooms.pop()
+                }
+            }
+            console.log(date_time_frame)
+            Axios.post('http://127.0.0.1:5000/coqui-bookings/Room/rooms/verifytimeframe', date_time_frame)
+                .then(function (response) {
+                    console.log(response.data);
+                    let room = response.data;
+                    for (let i = 0; i < room.length; i++) {
+                        rooms.push({
+                            'room_id': room[i].room_id,
+                            'room_name': room[i].room_name,
+                            'room_type_id': room[i].room_type_id
+                        })
+                    }
+                    textarea.value = JSON.stringify(rooms)
+                }).catch(
+                err => {
+                    console.log(err)
+                })
+        }
+
 
         return (<Segment>
                 <Grid columns={2} relaxed='very' stackable>
@@ -119,7 +144,79 @@ import Axios from "axios";
                         placeholder="Available Rooms"
                         disabled
                     />
+                    <hr/>
+                <Grid columns={2} relaxed='very' stackable>
+                    <Grid.Column>
+                        <h2>Find User Who Appointed A Room At A Given Time Frame:</h2>
+                        <Form>
+                            <Form.Input
+                                id='booking_start_date'
+                                icon='date'
+                                iconPosition='left'
+                                label='Start Date'
+                                placeholder='date'
+                                type='date'
+                                onChange={(event) => {
+                                    set_booking_start_date(event.target.value);
+                                }}
+                            />
+                            <Form.Input
+                                id='booking_start_time'
+                                icon='time'
+                                ampm={false}
+                                iconPosition='left'
+                                label='Start Time'
+                                placeholder='00:00'
+                                //type='time'
+                                onChange={(event) => {
+                                    set_booking_start_time(event.target.value);
+                                }}
+                            />
+                            <Form.Input
+                                id='booking_finish_date'
+                                icon='date'
+                                iconPosition='left'
+                                label='Finish Date'
+                                placeholder='date'
+                                type='date'
+                                onChange={(event) => {
+                                    set_booking_finish_date(event.target.value);
+                                }}
+                            />
+                            <Form.Input
+                                id="booking_finish_time"
+                                icon='time'
+                                ampm={false}
+                                iconPosition='left'
+                                label='Finish Time'
+                                placeholder='00:00'
+                                //type='time'
+                                onChange={(event) => {
+                                    set_booking_finish_time(event.target.value);
+                                }}
+                            />
+                        </Form>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Form>
+                            <Button content='Get Available Rooms' style={{marginTop: 28}} primary
+                                    onClick={getRoomsAvailableAtTimeFrame}/>
+                        </Form>
+                    </Grid.Column>
+                </Grid>
+                <TextArea
+                    id={'textarea'}
+                    style={{
+                        cursor: "text",
+                        width: "48%",
+                        height: "50%"
+                    }}
+                    value={rooms}
+                    placeholder="Available Rooms"
+                    disabled
+                />
             </Segment>
+
         )
     }
 export default RoomManagement
