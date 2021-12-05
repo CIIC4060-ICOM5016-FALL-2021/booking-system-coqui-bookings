dbe4320398c70f1567c023f38412c1f2900a607c
Ronald Velez <ronald.velez@upr.edu>
2021-12-04 23:56:56 -0400

    Clean Schedule.js

--- frontend/src/Schedule.js
+++ frontend/src/Schedule.js
@@ -5,18 +5,8 @@ import moment from 'moment';
 import {Button, Card, Container, Form, Grid, Modal, Segment} from "semantic-ui-react";
 import axios from "axios";
 
-moment.locale("en-GB");
-const localizer = momentLocalizer(moment);
-
-
 function Schedule() {
     const evs = []
-    const [dates, setDates] = useState([{
-        'title': 'Selection',
-        'allDay': false,
-        'start': new Date(moment.now()),
-        'end': new Date(moment.now())
-    }]);
     const data = {
         user_id: localStorage.getItem("user_id")
     }
@@ -39,7 +29,6 @@ function Schedule() {
                     console.log("Error:" + err)
                 })
 
-    //const [open, setOpen] = useState(false);
     const localizer = momentLocalizer(moment)
 
     return <Container style={{ height: 800 }}><Calendar
@@ -54,97 +43,5 @@ function Schedule() {
     </Calendar>
     </Container>
 
-        )
-    }
 }
 export default Schedule;
-
-    // const [open, setOpen] = useState(false);
-    // const localizer = momentLocalizer(moment)
-
-//     return (<Container style={{ height: 800 }}><Calendar
-//         localizer={localizer}
-//         startAccessor="start"
-//         events={""}
-//         endAccessor="end"
-//         views={["month", "day"]}
-//         defaultDate={Date.now()}
-//     >
-//
-//     </Calendar>
-//         <Button
-//             fluid
-//             onClick={useShowUserSchedule}
-//         >Load Schedule </Button>
-//     </Container>
-//
-// )
-//}
-//export default Schedule;
-
-
-// import React, {Component, useState} from 'react';
-// import {Calendar, dateFormat, momentLocalizer, Views} from 'react-big-calendar';
-// import 'react-big-calendar/lib/css/react-big-calendar.css';
-// import moment from 'moment';
-// import {Button, Card, Container, Form, Grid, Modal, Segment} from "semantic-ui-react";
-// import Axios from "axios";
-//
-// function Schedule(){
-//
-//     const [date, set_date] = useState("");
-//
-//     const data = {
-//         date : date,
-//         user_id: localStorage.getItem("user_id")
-//     }
-//     async function getUserSchedule(){
-//         const schedule = await Axios.get('https://coqui-bookings-database.herokuapp.com/coqui-bookings/User/unavailable-time-users/' + data.user_id)
-//             .then(res => {
-//                 console.log(res.data)
-//                 return res.data
-//
-//             }).catch(err => {
-//                 return "NOT FOUND ERROR";
-//             })
-//         return schedule;
-//     }
-//
-//     async function useShowUserSchedule() {
-//         let [events, setEvents] = useState([]);
-//         const user_schedule = await getUserSchedule();
-//         for(const k in user_schedule){
-//             events.push(new Event("title", user_schedule[k].unavailable_time_ser_start, user_schedule[k].unavailable_time_user_finish));
-//         }
-//         return events;
-//     }
-//
-//     // const [events, setEvents] = useState([{
-//     //     'title':'Unavailable',
-//     //     'allDay': false,
-//     //     'start': new Date(moment.now()),
-//     //     'end': new Date(moment.now())
-//     // }]);
-//     const test = useShowUserSchedule();
-//     const [open, setOpen] = useState(false);
-//     const localizer = momentLocalizer(moment)
-//
-//     return <Container style={{ height: 800 }}><Calendar
-//         localizer={localizer}
-//         startAccessor="start"
-//         events={""}
-//         endAccessor="end"
-//         views={["month", "day"]}
-//         defaultDate={Date.now()}
-//     >
-//
-//     </Calendar>
-//         <Button
-//             fluid
-//             onClick={useShowUserSchedule}
-//         >Load Schedule </Button>
-//     </Container>
-//
-//
-// }
-// export default Schedule;
