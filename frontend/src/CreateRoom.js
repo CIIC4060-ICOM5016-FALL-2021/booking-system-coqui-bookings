1d936d53a54c5c71422cbd18b67610a555623e0b
kathiana.diaz <kathiana.diaz@upr.edu>
2021-12-05 20:30:53 -0800

    add update room to frontend

--- frontend/src/CreateRoom.js
+++ frontend/src/CreateRoom.js
@@ -6,11 +6,12 @@ function CreateRoom() {
 
     const [room_name, set_room_name] = useState("");
     const [room_type_id, set_room_type_id] = useState("");
+    const [room_id, set_room_id] = useState("");
+
 
     const createRoom = event => {
         event.preventDefault();
-
-        const user_id = localStorage.getItem("user_id")
+        
         const data = {
             room_name: room_name,
             room_type_id: room_type_id,
@@ -22,9 +23,28 @@ function CreateRoom() {
                 console.log(res)
             }).catch(
             err => {
-                console.log("Error:" + err)
+                console.log(err)
             })
     }
+
+    const updateRoom = event => {
+        event.preventDefault();
+
+        const data = {
+            room_name: room_name,
+            room_type_id: room_type_id,
+        };
+        console.log(data)
+        Axios.post(`https://coqui-bookings-database.herokuapp.com/coqui-bookings/Room/rooms/${room_id}`, data).then(
+            res => {
+                window.alert("Room updated successfully.")
+                console.log(res)
+            }).catch(
+            err => {
+                console.log(err)
+            })
+    }
+
     return (<Segment>
                 <Grid columns={2} relaxed='very' stackable>
                     <Grid.Column>
@@ -53,7 +73,44 @@ function CreateRoom() {
                         <Button content={'Create Room'} primary  onClick={createRoom}/>
                     </Grid.Column>
                 </Grid>
-        </Segment>
+            <hr/>
+        <Grid columns={2} relaxed='very' stackable>
+            <Grid.Column>
+                <Form>
+                    <Form.Input
+                        id='room_name'
+                        icon='room'
+                        iconPosition='left'
+                        label='Room Name'
+                        type='text'
+                        onChange={(event) => {
+                            set_room_name(event.target.value);}}
+                    />
+                    <Form.Input
+                        id='room_type_id'
+                        icon='room'
+                        type={'number'}
+                        iconPosition='left'
+                        label='Room Type Id'
+                        onChange={(event) => {
+                            set_room_type_id(event.target.value);}}
+                    />
+                    <Form.Input
+                        id='room_id'
+                        icon='room'
+                        type={'number'}
+                        iconPosition='left'
+                        label='Room Id'
+                        onChange={(event) => {
+                            set_room_id(event.target.value);}}
+                    />
+                </Form>
+            </Grid.Column>
+            <Grid.Column>
+                <Button content={'Update Room'} primary  onClick={updateRoom}/>
+            </Grid.Column>
+        </Grid>
+    </Segment>
     )
 }
 export default CreateRoom;
