4ba1a799add0902b1afd4e8dc51ba86e726f1973
kathiana.diaz <kathiana.diaz@upr.edu>
2021-12-06 10:52:20 -0800

    update to dashboard

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -18,26 +18,48 @@ function Dashboard(){
                                                 // {"name": 4, "Counts": 2},
                                                 // {"name": 5, "Counts": 1}]);
     const data_busiest_times = []
+    const data_most_booked_user = []
+    const data_most_booked_room = []
 
     Axios.get(`https://coqui-bookings-database.herokuapp.com/coqui-bookings/Booking/bookings/busiest-times`)
         .then(function (response) {
-            //console.log(response.data);
             let busiest_times = response.data;
-            //data_busiest_times = []
             for (let i = 0; i < busiest_times.length; i++) {
                 data_busiest_times.push({
                     "name" : busiest_times[i].start_time+"-"+busiest_times[i].finish_time,
                     "times_booked": busiest_times[i].times_booked
                 })
             }
-            //localStorage.setItem('most_booked',JSON.stringify(data_busiest_times))
-
         }).catch(
             err => {
                 console.log(err)
     })
-    
-    console.log(data_busiest_times)
+    Axios.get(`https://coqui-bookings-database.herokuapp.com/coqui-bookings/Booking/bookings/most-booked-users`)
+        .then(function (response) {
+            let most_booked = response.data;
+            for (let i = 0; i < most_booked.length; i++) {
+                data_most_booked_user.push({
+                    "user_id" : most_booked[i].user_id,
+                    "times_booked": most_booked[i].times_booked
+                })
+            }
+        }).catch(
+            err => {
+                console.log(err)
+    })
+    Axios.get(`https://coqui-bookings-database.herokuapp.com/coqui-bookings/Booking/bookings/most-booked-rooms`)
+    .then(function (response) {
+        let most_booked = response.data;
+        for (let i = 0; i < most_booked.length; i++) {
+            data_most_booked_room.push({
+                "room_id" : most_booked[i].user_id,
+                "times_booked": most_booked[i].times_booked
+            })
+        }
+    }).catch(
+        err => {
+            console.log(err)
+    })
     
     return <Container style={{ height: 800 }}>
 
@@ -48,6 +70,22 @@ function Dashboard(){
             <Tooltip />
             <Legend />
             <Bar dataKey="times_booked" fill="#8884d8" />
+        </BarChart>
+        <BarChart width={730} height={250} data={data_most_booked_user}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="most_booked_user" fill="#8884d8" />
+        </BarChart>
+        <BarChart width={730} height={250} data={data_most_booked_room}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="most_booked_room" fill="#8884d8" />
         </BarChart>
     </Container>
 
