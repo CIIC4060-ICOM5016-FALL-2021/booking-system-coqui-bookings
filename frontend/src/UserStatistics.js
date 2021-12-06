7f9fef0db5f590e1b72ba328bb6069c8aa4a4031
kathiana.diaz <kathiana.diaz@upr.edu>
2021-12-05 17:27:05 -0800

    dashboard updates

--- frontend/src/UserStatistics.js
+++ frontend/src/UserStatistics.js
@@ -12,43 +12,27 @@ function UserStatistics(){
 
     const user_id=localStorage.getItem("user_id")
 
-    // const [invitee_id, set_invitee_id] = useState("");
-    // const [room_id, set_room_id] = useState("");
-    // const invitee_id = ""
-    // const room_id = ""
-
-    //const get_invitee_id = () => {
-
         Axios.get(`https://coqui-bookings-database.herokuapp.com/coqui-bookings/User/users/${user_id}/invitee-most-booked-with`)
             .then(function (response) {
                 console.log(response.data);
-                //set_invitee_id(response.data);
                 let invitee_id=response.data.user_first_name +" "+ response.data.user_last_name
                 localStorage.setItem('invitee_id',invitee_id)
-                //return invitee_id
 
             }).catch(
                 err => {
                     console.log("Error:" + err)
             })
-    //}
 
-    //const get_room_id = () => {
         Axios.get("https://coqui-bookings-database.herokuapp.com/coqui-bookings/User/users/"+ user_id +"/most_used_room")
             .then(function (response) {
                 console.log(response.data);
                 let room_id = response.data.room_name
                 localStorage.setItem('room_id', room_id)
 
-                // set_room_id(response.data);  
-                // room_id=response.data
-                // return room_id
-
             }).catch(
                 err => {
                     console.log("Error:" + err)
             })
-        //}
 
     return (<Container style={{ height: 800 }}>
 
